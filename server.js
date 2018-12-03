ad20b1fcfff259e720605d24a583f8d3e305fb3c
unknown <ewilsons@aol.com>
2018-12-03 10:10:06 -0500

    readme/config

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-// *****************************************************************************
+// *********************************************************************************
 // Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
+// *********************************************************************************
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
@@ -11,20 +8,18 @@ var session = require("express-session");
 var passport = require("./config/passport");
 
 // Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080; //used for heroku otherwise will run on 8080
 
 // Requiring our models for syncing
 var db = require("./models"); //models folder created by sequelize command line
-//includes index.js and any additional tables
-
-// Sets up the Express app to handle data parsing
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); //middleware code comes from bodyparser package
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // parse application/json
 app.use(bodyParser.json());
+
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
@@ -32,19 +27,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Static directory
-app.use(express.static("public")); //basically assets folder
+// Static directory(assets)
+app.use(express.static("public")); 
 
+// Setup handelbars view engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
 // Routes
-// =============================================================
 require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
 require("./routes/user-apiRoutes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
-// =============================================================
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
